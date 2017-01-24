c804c2458f5997ef34fc45c301ce367cebfad8b0
Qawery <wooki1@op.pl>
2017-01-24 18:03:07 +0100

    usunieto pliki CLion z git

--- cmake-build-debug/CMakeFiles/skeleton_transforms.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for skeleton_transforms.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wooki/CatkinWorkspace/src/elektron_escort
-# Build directory: /home/wooki/CatkinWorkspace/src/elektron_escort/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
